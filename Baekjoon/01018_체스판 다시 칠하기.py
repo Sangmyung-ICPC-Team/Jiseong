0d474fd84dc36883c9f08400ad7800c745162ce6
hanjiseong <55674646+tjdwl3269@users.noreply.github.com>
2021-06-03 19:15:31 +0900

    Sliver 5

---
+++ Baekjoon/01018_체스판 다시 칠하기.py
@@ -0,0 +1,24 @@
+N, M = map(int, input().split())
+board = list()
+for i in range(N):
+    board.append(input())
+answer = list()
+for i in range(N-7):
+    for j in range(M-7):
+        f_W, f_B = 0, 0
+        for k in range(i,i+8):
+            for l in range(j,j + 8):
+                if (k + l) % 2 == 0:
+                    if board[k][l] != 'W':
+                        f_W = f_W + 1
+                    if board[k][l] != 'B':
+                        f_B = f_B + 1
+                else:
+                    if board[k][l] != 'B':
+                        f_W = f_W + 1
+                    if board[k][l] != 'W':
+                        f_B = f_B + 1
+        answer.append(f_W)
+        answer.append(f_B)
+        
+print(min(answer))
