a72d6c105fc08cd3020ff9d05d4ab46ff48a5b72
hanjiseong <55674646+tjdwl3269@users.noreply.github.com>
2021-07-05 19:28:54 +0900

    Silver 1

---
+++ Baekjoon/01011_FlymetotheAlphaCentauri.py
@@ -0,0 +1,20 @@
+# Silver 1
+
+t = int(input())
+
+for _ in range(t):
+    x, y = map(int,input().split())
+    distance = y - x
+    count = 0  
+    move = 1  
+    move_plus = 0  
+    while move_plus < distance :
+        count += 1
+        move_plus += move  
+        if count % 2 == 0 :  
+            move += 1  
+    print(count)
+
+
+
+
